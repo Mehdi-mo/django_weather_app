00d76398f34997be7060eb08a76776605b98ef5d
mehdi mosayebi <mosayebimehdi13782000@gmail.com>
2024-02-06 12:44:59 +0330

    adding error handling for api and showing api data in home template

--- weather/views.py
+++ weather/views.py
@@ -2,7 +2,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'weather/home.html')
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=45EB9E13-ED62-47F8-8C8E-623A1963EE63")
+
+
+    try:
+        api = json.loads(api_request.content)
+
+    except Exception as e:
+        api = "Error..."
+
+
+    # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=45EB9E13-ED62-47F8-8C8E-623A1963EE63
+    return render(request, 'weather/home.html', {'api': api})
+
 
 def aboutus_view(request):
     return render(request, 'aboutus.html')
