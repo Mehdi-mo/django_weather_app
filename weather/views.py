0bb3681f4d8845f0a275f3fe5d9344486bb1756e
mehdi mosayebi <mosayebimehdi13782000@gmail.com>
2024-02-10 14:21:20 +0330

    adding post request for zipcode

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,14 @@ def home(request):
     import json
     import requests
 
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=45EB9E13-ED62-47F8-8C8E-623A1963EE63")
+    
+    
+    if request.method == 'POST':
+        zipcode = request.POST['zipcode']
+
+    
+
+    api_request = requests.get(f"https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zipcode }&distance=5&API_KEY=45EB9E13-ED62-47F8-8C8E-623A1963EE63")
 
 
     try:
@@ -14,9 +21,12 @@ def home(request):
     except Exception as e:
         api = "Error..."
 
+    return render(request, 'weather/home.html', {'api': api, "zipcode": zipcode})
+
+
 
     # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=45EB9E13-ED62-47F8-8C8E-623A1963EE63
-    return render(request, 'weather/home.html', {'api': api})
+    
 
 
 def aboutus_view(request):
